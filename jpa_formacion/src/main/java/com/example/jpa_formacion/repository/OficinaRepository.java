ef8b8c155d64666a218ea350b3bc92a06424874c
Beatriz Bello <116346970+beatrizbellosan@users.noreply.github.com>
2022-12-14 19:18:19 +0100

    Delete jpa_formacion directory

--- jpa_formacion/src/main/java/com/example/jpa_formacion/repository/OficinaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpa_formacion.repository;
-
-import com.example.jpa_formacion.model.Oficina;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OficinaRepository extends JpaRepository<Oficina,Long> {
-}
