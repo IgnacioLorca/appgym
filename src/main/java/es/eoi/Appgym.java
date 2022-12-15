7a5000c6225943d9b0598c464b52b8e3d2aba034
Beatriz Bello <beatrizbel.san@gmail.com>
2022-12-15 18:40:37 +0100

    Actualización1
    Actualización del archivo main para poder ejecutarlo y cambio de nombre de database en application.properties

---
+++ src/main/java/es/eoi/Appgym.java
@@ -0,0 +1,9 @@
+package es.eoi;
+
+import org.springframework.boot.SpringApplication;
+
+public class Appgym {
+    public static void main(String[] args) {
+        SpringApplication.run(Appgym.class, args);
+    }
+}
