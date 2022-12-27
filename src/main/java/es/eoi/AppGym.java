59629db8079dca30c8e6b836a076346ec8dc8c42
Beatriz Bello <beatrizbel.san@gmail.com>
2022-12-27 20:03:52 +0100

    Update

---
+++ src/main/java/es/eoi/AppGym.java
@@ -0,0 +1,11 @@
+package es.eoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appgym {
+    public static void main(String[] args) {
+        SpringApplication.run(Appgym.class, args);
+    }
+}
