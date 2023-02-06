6424c8384499b081d5b1bab054bb3a6ef4f830fd
aanillo <94526136+aanillo@users.noreply.github.com>
2023-02-06 10:17:31 +0100

    Código actualizado, controllers añadidos, no funciona seguridad

---
+++ src/test/java/es/eoi/AppGymTest.java
@@ -0,0 +1,11 @@
+package es.eoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppGymTest {
+    @Test
+    void contextLoads() {
+    }
+}
